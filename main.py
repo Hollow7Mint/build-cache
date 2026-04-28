434885cb69371e196e88631d21c1ae1ce402d902
James Holloway <james.holloway94@gmail.com>
2026-04-28 21:29:19 +0000

    document ssh host key sync time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-28 20:51:02 UTC
+# Last sync: 2026-04-28 21:29:19 UTC
