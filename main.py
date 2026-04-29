058ce0290763c4c0271d4d488e49af5b07492bca
James Holloway <james.holloway94@gmail.com>
2026-04-29 07:48:57 +0000

    audit vpc peering sync record after route update

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-29 07:05:33 UTC
+# Last sync: 2026-04-29 07:48:57 UTC
