b3014a763b5cccc13623b6ea1d321e88ca750f52
James Holloway <james.holloway94@gmail.com>
2026-04-30 16:33:36 +0000

    tune bastion audit log sync time for review

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-04-30 15:26:03 UTC
+# Last sync: 2026-04-30 16:33:36 UTC
