4c2aeaed5ed1c86d1b49aa59f4557b5a5dbbbb78
James Holloway <james.holloway94@gmail.com>
2026-05-03 03:40:57 +0000

    store cloudfront ttl sync stamp for cache audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-03 02:13:16 UTC
+# Last sync: 2026-05-03 03:40:57 UTC
