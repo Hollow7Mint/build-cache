e818a8cee6b68b87635e2ad4f7ef50d8a9508a5a
James Holloway <james.holloway94@gmail.com>
2026-05-03 14:26:57 +0000

    tune mongodb replication lag time for audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-03 13:18:48 UTC
+# Last sync: 2026-05-03 14:26:57 UTC
