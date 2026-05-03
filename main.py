4f99753ef7cf5cc95d462f478da5b2a7bd431948
James Holloway <james.holloway94@gmail.com>
2026-05-03 16:01:06 +0000

    stage postgres tablespace sync for scheduled refresh

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-03 14:26:57 UTC
+# Last sync: 2026-05-03 16:01:06 UTC
