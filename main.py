95f4e649b6ef63209e66bafb8c43be1b2bef07b7
James Holloway <james.holloway94@gmail.com>
2026-05-06 11:13:08 +0000

    align vpc subnet allocation time in config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-06 10:38:01 UTC
+# Last sync: 2026-05-06 11:13:08 UTC
