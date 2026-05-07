ab1f5a5e8c9cb0951f3152fd702ce0b26b143bcf
James Holloway <james.holloway94@gmail.com>
2026-05-07 09:35:43 +0000

    bridge lambda env var sync through config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-07 09:06:03 UTC
+# Last sync: 2026-05-07 09:35:43 UTC
