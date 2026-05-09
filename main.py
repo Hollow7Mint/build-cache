c258858753809b47f768fa81cf117f94f37cc46d
James Holloway <james.holloway94@gmail.com>
2026-05-09 10:31:15 +0000

    frame mysql connection limit sync in config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-09 09:54:16 UTC
+# Last sync: 2026-05-09 10:31:15 UTC
