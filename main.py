009a3e8362979dcf28c21bfa1454b6d2da877427
James Holloway <james.holloway94@gmail.com>
2026-05-09 03:22:52 +0000

    lift asg capacity sync time into deploy banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-09 02:34:55 UTC
+# Last sync: 2026-05-09 03:22:52 UTC
