ae6077787b9d5f57252b834ddbcfa598217975fd
James Holloway <james.holloway94@gmail.com>
2026-05-09 04:55:17 +0000

    log shield ddos event time in protection banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-09 03:22:52 UTC
+# Last sync: 2026-05-09 04:55:17 UTC
