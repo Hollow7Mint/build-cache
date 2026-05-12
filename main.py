231dc370c7f98fbb4b2136eca22f3ce3234de580
James Holloway <james.holloway94@gmail.com>
2026-05-12 22:09:13 +0000

    capture db backup window completion time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 21:31:05 UTC
+# Last sync: 2026-05-12 22:09:13 UTC
