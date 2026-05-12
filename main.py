75f66922acf0171570f1f8ce9ed4e9fa29539bc0
James Holloway <james.holloway94@gmail.com>
2026-05-12 11:54:30 +0000

    flush bastion authorized_keys time for audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 10:48:29 UTC
+# Last sync: 2026-05-12 11:54:30 UTC
