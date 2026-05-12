bfd2aa904205304870f2164d2ec9f49e0702b6ea
James Holloway <james.holloway94@gmail.com>
2026-05-12 05:03:11 +0000

    rebuild nat gateway sync record after route change

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-12 03:46:02 UTC
+# Last sync: 2026-05-12 05:03:11 UTC
