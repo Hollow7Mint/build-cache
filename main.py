d6811c825912655ada14caa80b6b43cb705c0c21
James Holloway <james.holloway94@gmail.com>
2026-05-17 04:25:16 +0000

    write bastion access log time into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-17 03:35:47 UTC
+# Last sync: 2026-05-17 04:25:16 UTC
