443e42e34a70840c6099d38779f1bcd897d788ad
James Holloway <james.holloway94@gmail.com>
2026-05-19 00:48:26 +0000

    queue redis keyspace sync after config update

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-18 23:34:19 UTC
+# Last sync: 2026-05-19 00:48:26 UTC
