263e61ac88b33e6c46e65852eba6d8315dd03eab
James Holloway <james.holloway94@gmail.com>
2026-05-19 18:42:02 +0000

    load mysql slow query log sync from pipeline

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-19 17:24:41 UTC
+# Last sync: 2026-05-19 18:42:02 UTC
