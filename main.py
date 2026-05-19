ba87ff2b4b006d11eaf80cc8814b7abc77181d72
James Holloway <james.holloway94@gmail.com>
2026-05-19 02:43:51 +0000

    inspect postgres lock wait sync for performance

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-19 01:44:28 UTC
+# Last sync: 2026-05-19 02:43:51 UTC
