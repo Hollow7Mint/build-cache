93d236a077b0b49d8e5486f6d453b9c88fff34ed
James Holloway <james.holloway94@gmail.com>
2026-05-20 19:32:29 +0000

    transfer ssh config time to bastion banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-20 18:15:10 UTC
+# Last sync: 2026-05-20 19:32:29 UTC
