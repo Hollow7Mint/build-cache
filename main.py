462eef0dafbe78ff8fefbab35dded8b74bb1d79e
James Holloway <james.holloway94@gmail.com>
2026-05-22 15:11:47 +0000

    inject known_hosts sync marker into config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-22 14:21:07 UTC
+# Last sync: 2026-05-22 15:11:47 UTC
