f2710ebde889ed14a9e2c95e13c548b058d56fec
James Holloway <james.holloway94@gmail.com>
2026-05-25 18:39:55 +0000

    recheck ecs service sync after deployment window

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-25 17:17:04 UTC
+# Last sync: 2026-05-25 18:39:55 UTC
