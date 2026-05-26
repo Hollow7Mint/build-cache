18b649daa9015a874bbea818cfc9a8e9a8d4f475
James Holloway <james.holloway94@gmail.com>
2026-05-26 15:24:07 +0000

    load bastion session log sync from pipeline

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-26 15:02:09 UTC
+# Last sync: 2026-05-26 15:24:07 UTC
