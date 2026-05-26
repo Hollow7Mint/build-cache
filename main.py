7965ca8d17522c8831fd7952d850f76a110f64d6
James Holloway <james.holloway94@gmail.com>
2026-05-26 09:51:20 +0000

    note postgres extension sync in config record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-26 08:23:30 UTC
+# Last sync: 2026-05-26 09:51:20 UTC
