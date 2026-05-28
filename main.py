37a5e2212457bf8a2cef8543cc2c58188ee5d94e
James Holloway <james.holloway94@gmail.com>
2026-05-28 23:56:13 +0000

    tag postgres checkpoint sync for replication trail

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-28 22:19:08 UTC
+# Last sync: 2026-05-28 23:56:13 UTC
