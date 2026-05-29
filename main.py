c49aa73092be9aa01215a44ebd46ed90644a1e36
James Holloway <james.holloway94@gmail.com>
2026-05-29 00:18:27 +0000

    harvest postgres vacuum sync for nightly audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-28 23:56:13 UTC
+# Last sync: 2026-05-29 00:18:27 UTC
