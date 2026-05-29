e8c941b882918c602103bce048f4e2508ecd2fc6
James Holloway <james.holloway94@gmail.com>
2026-05-29 04:28:18 +0000

    push secrets manager version stamp to metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-29 03:31:56 UTC
+# Last sync: 2026-05-29 04:28:18 UTC
