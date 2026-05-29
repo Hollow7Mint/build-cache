66071dd7e48e7c878b800ad1ecb4462da3f3f7c1
James Holloway <james.holloway94@gmail.com>
2026-05-29 21:24:36 +0000

    remap ec2 security group rule time in config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-05-29 21:07:02 UTC
+# Last sync: 2026-05-29 21:24:36 UTC
