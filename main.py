62f79e9c95838c2a211c9823bb5e74867bf89cbf
James Holloway <james.holloway94@gmail.com>
2026-06-01 16:25:16 +0000

    unify db sync record with metadata heartbeat

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-01 15:30:31 UTC
+# Last sync: 2026-06-01 16:25:16 UTC
