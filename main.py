3688f6fd92ecbfc96e3f0018da4828519bf8f692
James Holloway <james.holloway94@gmail.com>
2026-06-02 13:20:58 +0000

    finalize bastion config sync after nightly rotation

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-02 12:48:37 UTC
+# Last sync: 2026-06-02 13:20:58 UTC
