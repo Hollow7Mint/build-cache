9ddee907e7e115967ec44fcd88fef2c60c4fb378
James Holloway <james.holloway94@gmail.com>
2026-06-02 12:48:37 +0000

    bump known_hosts update stamp after rotation

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-02 11:59:31 UTC
+# Last sync: 2026-06-02 12:48:37 UTC
