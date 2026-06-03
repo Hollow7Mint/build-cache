48d9d5ac800ceb98cbacd3686d47cf91576d9e17
James Holloway <james.holloway94@gmail.com>
2026-06-03 22:26:47 +0000

    finalize vpc security group sync after rule push

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-03 21:13:46 UTC
+# Last sync: 2026-06-03 22:26:47 UTC
