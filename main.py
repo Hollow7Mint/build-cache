c4f5fd1a5512bdf5d4ff0e52cb69e95a0b776ab0
James Holloway <james.holloway94@gmail.com>
2026-06-04 10:18:58 +0000

    validate mysql replication time in cluster config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-04 09:39:32 UTC
+# Last sync: 2026-06-04 10:18:58 UTC
