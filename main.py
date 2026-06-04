b46a7ff015b82782ddde5e12bb789bbbee5d0c5a
James Holloway <james.holloway94@gmail.com>
2026-06-04 15:57:49 +0000

    seal postgres logical replication sync in banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-04 14:48:04 UTC
+# Last sync: 2026-06-04 15:57:49 UTC
