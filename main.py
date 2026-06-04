10e4738faa760047f2684c5cf8e48e64bee18989
James Holloway <james.holloway94@gmail.com>
2026-06-04 09:39:32 +0000

    catalog rds parameter group sync after maintenance

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-04 08:17:46 UTC
+# Last sync: 2026-06-04 09:39:32 UTC
