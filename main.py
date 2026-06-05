5d38ec4b5ff55d693f52475638232cf8e259f758
James Holloway <james.holloway94@gmail.com>
2026-06-05 04:15:17 +0000

    normalize ecs cluster config time in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-05 03:31:31 UTC
+# Last sync: 2026-06-05 04:15:17 UTC
