e943d478ac1844eef7f26971472941622d99d6b8
James Holloway <james.holloway94@gmail.com>
2026-06-05 11:32:23 +0000

    thread postgres wal sync through config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-05 10:11:53 UTC
+# Last sync: 2026-06-05 11:32:23 UTC
