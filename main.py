5ecfb03aa376f5a69ae50f483f661bbb38b8e020
James Holloway <james.holloway94@gmail.com>
2026-06-06 01:17:34 +0000

    pin bastion ip allowlist time in config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-06 00:16:17 UTC
+# Last sync: 2026-06-06 01:17:34 UTC
