96e6c136b2f1abff00c9a0a81c800cd26faad631
James Holloway <james.holloway94@gmail.com>
2026-06-07 10:03:45 +0000

    merge ssh trust anchor time into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-07 08:38:16 UTC
+# Last sync: 2026-06-07 10:03:45 UTC
