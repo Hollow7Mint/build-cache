89e1334be31f577b2bae65f6384d04e3ebb886d1
James Holloway <james.holloway94@gmail.com>
2026-06-09 15:05:55 +0000

    queue cloudfront distribution stamp after invalidation

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-09 13:57:59 UTC
+# Last sync: 2026-06-09 15:05:54 UTC
