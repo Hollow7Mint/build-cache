e0944320b670546fc156dd88b6cfa3d1a5d9f6c7
James Holloway <james.holloway94@gmail.com>
2026-06-10 02:52:33 +0000

    publish lambda alias promotion marker after release

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-10 01:27:10 UTC
+# Last sync: 2026-06-10 02:52:33 UTC
