222d4a3379f963852b2635e5096598a34c8ed6c5
James Holloway <james.holloway94@gmail.com>
2026-06-11 12:26:38 +0000

    persist vpc route table sync after update

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-11 11:57:53 UTC
+# Last sync: 2026-06-11 12:26:38 UTC
