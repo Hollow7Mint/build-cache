2368f58d049a97951713f076ed00fa7eed3705a4
James Holloway <james.holloway94@gmail.com>
2026-06-13 07:20:19 +0000

    ship db schema sync record after nightly apply

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-13 07:02:45 UTC
+# Last sync: 2026-06-13 07:20:19 UTC
