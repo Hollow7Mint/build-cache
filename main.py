9ec9be782074611fb0ff62c85297393b86252d14
James Holloway <james.holloway94@gmail.com>
2026-06-14 11:04:17 +0000

    retain cloudwatch metric filter time for audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-14 09:30:47 UTC
+# Last sync: 2026-06-14 11:04:17 UTC
