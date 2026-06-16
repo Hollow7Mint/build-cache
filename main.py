e28f0b215b639e871c45c8b0e7ee1413c5d71cdd
James Holloway <james.holloway94@gmail.com>
2026-06-16 15:35:59 +0000

    mirror redis cluster node time in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-16 14:47:24 UTC
+# Last sync: 2026-06-16 15:35:59 UTC
