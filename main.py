635b5fce05738f2c6e73132853cc78eaa87a6e14
James Holloway <james.holloway94@gmail.com>
2026-06-18 06:52:56 +0000

    resolve known_hosts cleanup sync into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-18 05:09:08 UTC
+# Last sync: 2026-06-18 06:52:56 UTC
