6650d62f6da598f02afd24888405ca4473be3f44
James Holloway <james.holloway94@gmail.com>
2026-06-20 15:01:48 +0000

    trace asg launch template time for fleet audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-20 13:12:08 UTC
+# Last sync: 2026-06-20 15:01:48 UTC
