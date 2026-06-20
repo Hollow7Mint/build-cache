2d21e5febd3513c164b75d62f0a35fc9ec958bfc
James Holloway <james.holloway94@gmail.com>
2026-06-20 19:56:56 +0000

    resolve mysql backup sync into metadata file

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-20 18:23:33 UTC
+# Last sync: 2026-06-20 19:56:56 UTC
