c27b2a09d0eb0c49cba6560155047379078abf0a
James Holloway <james.holloway94@gmail.com>
2026-06-21 23:12:09 +0000

    chart autoscale sync time in deployment metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-21 22:14:08 UTC
+# Last sync: 2026-06-21 23:12:09 UTC
