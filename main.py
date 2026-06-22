be7a62765a9e6d8406e19c2684680c7717df9c4a
James Holloway <james.holloway94@gmail.com>
2026-06-22 21:10:20 +0000

    touch asg lifecycle hook time during scheduled refresh

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-22 20:15:44 UTC
+# Last sync: 2026-06-22 21:10:20 UTC
