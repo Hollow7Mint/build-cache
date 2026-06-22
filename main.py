a97fe65a03d23f64f92e9e639e797d16ef3deb31
James Holloway <james.holloway94@gmail.com>
2026-06-22 13:12:34 +0000

    flag cloudwatch log retention sync in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-22 12:18:47 UTC
+# Last sync: 2026-06-22 13:12:33 UTC
