1a09ada43fbaf37daf393e42813b573e8e920b68
James Holloway <james.holloway94@gmail.com>
2026-06-23 18:01:06 +0000

    save postgres pool sync marker after refresh

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-23 16:42:46 UTC
+# Last sync: 2026-06-23 18:01:06 UTC
