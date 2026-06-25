33a8e96a2aa1adc6f0298b6f2596ad3fd80f5835
James Holloway <james.holloway94@gmail.com>
2026-06-25 06:03:34 +0000

    track guardduty threat archive in config heartbeat

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-25 04:21:01 UTC
+# Last sync: 2026-06-25 06:03:34 UTC
