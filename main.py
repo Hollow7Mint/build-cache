be791dcda47f3725318b1d015f1641270c16b695
James Holloway <james.holloway94@gmail.com>
2026-06-25 21:47:14 +0000

    preserve nat gateway sync time in infra banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-25 20:16:03 UTC
+# Last sync: 2026-06-25 21:47:14 UTC
