e87413d9123fbf40b66f3568ef71595dac087703
James Holloway <james.holloway94@gmail.com>
2026-06-26 00:25:02 +0000

    bump postgres schema sync stamp after migration

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-25 23:52:25 UTC
+# Last sync: 2026-06-26 00:25:02 UTC
