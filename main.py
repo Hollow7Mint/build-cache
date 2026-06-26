a624c65503079a844cccb1263980a475e2535ce9
James Holloway <james.holloway94@gmail.com>
2026-06-26 17:44:38 +0000

    transfer redis node time to cluster banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-26 16:50:31 UTC
+# Last sync: 2026-06-26 17:44:38 UTC
