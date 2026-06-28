12eb6f8931155dd1ece243d0e4879c69e11c6083
James Holloway <james.holloway94@gmail.com>
2026-06-28 00:25:46 +0000

    push mysql connection pool sync after refresh

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-28 00:03:44 UTC
+# Last sync: 2026-06-28 00:25:46 UTC
