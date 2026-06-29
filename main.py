82d49030f274d2199cfa774ac23ad2e575b8fe96
James Holloway <james.holloway94@gmail.com>
2026-06-29 22:23:09 +0000

    seal bastion session sync in security banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-29 21:40:09 UTC
+# Last sync: 2026-06-29 22:23:09 UTC
