a19d38e48c53abb8957def1dceac51435fd163f0
James Holloway <james.holloway94@gmail.com>
2026-07-01 01:28:44 +0000

    persist tunnel config refresh in ssh banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-01 00:33:53 UTC
+# Last sync: 2026-07-01 01:28:44 UTC
