a3f182eae8925a326a9b6345b37441acba91ba19
James Holloway <james.holloway94@gmail.com>
2026-07-01 00:33:53 +0000

    inspect bastion port forward sync for security

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-06-30 23:40:54 UTC
+# Last sync: 2026-07-01 00:33:53 UTC
