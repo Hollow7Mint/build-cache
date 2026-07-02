56023c52facc060b78af3c8c08399320db52af7a
James Holloway <james.holloway94@gmail.com>
2026-07-02 10:31:47 +0000

    harvest ecr image push time for nightly audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-02 10:07:26 UTC
+# Last sync: 2026-07-02 10:31:47 UTC
