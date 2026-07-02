22c544f3c118f08a1b02979a501ef40e29731667
James Holloway <james.holloway94@gmail.com>
2026-07-02 13:17:59 +0000

    ship ssh key sync record after nightly apply

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-02 12:12:03 UTC
+# Last sync: 2026-07-02 13:17:59 UTC
