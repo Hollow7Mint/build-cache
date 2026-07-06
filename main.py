304ae8feebd92a9a3e06239c270f33a811c1cb75
James Holloway <james.holloway94@gmail.com>
2026-07-06 16:02:38 +0000

    annotate ec2 ami bake time for deploy record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-06 15:06:19 UTC
+# Last sync: 2026-07-06 16:02:38 UTC
