8b407b069e4401c186efce19972d43275a82f9e2
James Holloway <james.holloway94@gmail.com>
2026-07-07 17:05:48 +0000

    write mongodb index rebuild time into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-07 15:26:09 UTC
+# Last sync: 2026-07-07 17:05:48 UTC
