7d0c1a18d5d8b42b2dc0b5946775c281f22c85d7
James Holloway <james.holloway94@gmail.com>
2026-07-10 20:02:29 +0000

    annotate mysql slow log sync time for audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-10 19:07:03 UTC
+# Last sync: 2026-07-10 20:02:29 UTC
