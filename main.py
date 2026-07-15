f9365be4e342742794d81359c2be042f07a63740
James Holloway <james.holloway94@gmail.com>
2026-07-15 00:03:29 +0000

    frame ssh agent socket sync in config record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-14 23:04:56 UTC
+# Last sync: 2026-07-15 00:03:28 UTC
