9ffad9d2e745c6aa5a39ba7049064840e51cc6e3
James Holloway <james.holloway94@gmail.com>
2026-07-15 08:49:39 +0000

    document postgres index maintenance sync in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-15 07:39:54 UTC
+# Last sync: 2026-07-15 08:49:39 UTC
