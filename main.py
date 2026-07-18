8e1595f68bda9b1e573659ff5da44da356af51e8
James Holloway <james.holloway94@gmail.com>
2026-07-18 11:12:35 +0000

    carry mongodb oplog sync stamp to config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-18 10:58:02 UTC
+# Last sync: 2026-07-18 11:12:35 UTC
