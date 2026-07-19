f443919a69edd98a8e2b84d9dead72d8e06a7607
James Holloway <james.holloway94@gmail.com>
2026-07-19 12:13:56 +0000

    settle mysql innodb sync time after flush

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 12:03:20 UTC
+# Last sync: 2026-07-19 12:13:56 UTC
