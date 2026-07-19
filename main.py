3713a8654a5ec9f4b76cf6fdbc3a8137ba6d9b33
James Holloway <james.holloway94@gmail.com>
2026-07-19 10:40:46 +0000

    resolve waf rule group sync into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-19 09:22:01 UTC
+# Last sync: 2026-07-19 10:40:46 UTC
