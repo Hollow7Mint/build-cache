7ee0e6d5a2a9e26066aa3ef91d77f945e584cf5d
James Holloway <james.holloway94@gmail.com>
2026-07-21 19:01:35 +0000

    inject ecs task definition marker into config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-21 17:22:13 UTC
+# Last sync: 2026-07-21 19:01:35 UTC
