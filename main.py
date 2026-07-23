6182edf05d7499ccea879efeebed218c722ffe4f
James Holloway <james.holloway94@gmail.com>
2026-07-23 18:36:59 +0000

    advance ssh key trust sync into config record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-23 17:29:07 UTC
+# Last sync: 2026-07-23 18:36:59 UTC
