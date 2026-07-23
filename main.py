0b0400e34c3c2a613791e0b05fbedba6821393ad
James Holloway <james.holloway94@gmail.com>
2026-07-23 05:34:53 +0000

    lift ssh certificate expiry time into banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-23 04:42:16 UTC
+# Last sync: 2026-07-23 05:34:53 UTC
