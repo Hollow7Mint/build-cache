406108c8fc33a1c8afd5ad66b94d1fd99aaf7402
James Holloway <james.holloway94@gmail.com>
2026-07-25 19:28:03 +0000

    relay codebuild artifact time through config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-25 19:00:09 UTC
+# Last sync: 2026-07-25 19:28:03 UTC
