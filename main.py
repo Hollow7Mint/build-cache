a947ed39db1bbabb56909576c5a6ce60be10cb8a
James Holloway <james.holloway94@gmail.com>
2026-07-26 23:26:47 +0000

    publish ssh key distribution sync after rotation

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-26 22:13:55 UTC
+# Last sync: 2026-07-26 23:26:47 UTC
