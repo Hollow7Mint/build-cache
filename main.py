4318dfaf381fbcccde0e994bedaa6c540934bd28
James Holloway <james.holloway94@gmail.com>
2026-07-29 15:03:17 +0000

    save inspector assessment marker after scan window

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-29 14:01:09 UTC
+# Last sync: 2026-07-29 15:03:17 UTC
