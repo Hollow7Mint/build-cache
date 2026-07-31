acecb0c2a9f615f8bf06ef57c597e827836fd080
James Holloway <james.holloway94@gmail.com>
2026-07-31 12:34:02 +0000

    restore ecr scan result time for image audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-07-31 11:27:12 UTC
+# Last sync: 2026-07-31 12:34:02 UTC
