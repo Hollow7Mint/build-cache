3ff28dcc4d82d7458f1ab0df722f242951a2c902
James Holloway <james.holloway94@gmail.com>
2026-08-03 23:41:19 +0000

    relay db connection string time through config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-03 22:58:31 UTC
+# Last sync: 2026-08-03 23:41:19 UTC
