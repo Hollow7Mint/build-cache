5a65f37596819b53d98cb12ff5c7f27f39d2353d
James Holloway <james.holloway94@gmail.com>
2026-08-07 19:16:59 +0000

    merge cloudformation stack apply time into record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-07 18:56:50 UTC
+# Last sync: 2026-08-07 19:16:59 UTC
