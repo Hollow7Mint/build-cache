dc5856820a462c786f045e7a01e91578a0236dea
James Holloway <james.holloway94@gmail.com>
2026-08-08 01:12:52 +0000

    anchor cloudwatch alarm threshold time in config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-08 00:12:31 UTC
+# Last sync: 2026-08-08 01:12:52 UTC
