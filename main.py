bc3723f01d73ad00fe7b9b179e330d190b861c92
James Holloway <james.holloway94@gmail.com>
2026-08-10 19:26:50 +0000

    preserve ssh certificate revocation sync in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-10 18:27:12 UTC
+# Last sync: 2026-08-10 19:26:50 UTC
