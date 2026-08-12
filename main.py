da35bf71183730cf6c6bab82c2db3287f418f760
James Holloway <james.holloway94@gmail.com>
2026-08-12 13:16:18 +0000

    preserve mysql user permission sync in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 13:00:01 UTC
+# Last sync: 2026-08-12 13:16:18 UTC
