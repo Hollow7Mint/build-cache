7715ddd9fb4eeb7abec2de6fdfe84c8bf5de01fc
James Holloway <james.holloway94@gmail.com>
2026-08-12 19:37:13 +0000

    pin mongodb shard balancing time in config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-12 18:46:30 UTC
+# Last sync: 2026-08-12 19:37:13 UTC
