9f1d8d10064fed8f2b5aa6216b70315dff9df750
James Holloway <james.holloway94@gmail.com>
2026-08-13 15:31:29 +0000

    adjust bastion acl sync stamp after ip review

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-13 14:54:50 UTC
+# Last sync: 2026-08-13 15:31:29 UTC
