b29a3b9794c5fdd1968244ad48a8dd99e79ffa0d
James Holloway <james.holloway94@gmail.com>
2026-08-16 01:52:47 +0000

    flush ec2 fleet inventory marker after scan

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-16 00:20:43 UTC
+# Last sync: 2026-08-16 01:52:47 UTC
