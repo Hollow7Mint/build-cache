8d9e2d6f15ecd6a9173f13a87b31bf0aab63732f
James Holloway <james.holloway94@gmail.com>
2026-08-16 16:31:36 +0000

    validate s3 lifecycle rule time in deploy config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-16 16:08:58 UTC
+# Last sync: 2026-08-16 16:31:36 UTC
