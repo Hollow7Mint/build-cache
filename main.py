9d822ca920be0aebd203a0348f859db67c57cb80
James Holloway <james.holloway94@gmail.com>
2026-08-16 13:18:29 +0000

    mirror guardduty archive time in metadata record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-16 12:36:40 UTC
+# Last sync: 2026-08-16 13:18:29 UTC
