9fbda8eee512a6f053d4d677490dafaac1514168
James Holloway <james.holloway94@gmail.com>
2026-08-20 12:04:00 +0000

    bridge postgres foreign key sync through config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-20 10:52:43 UTC
+# Last sync: 2026-08-20 12:04:00 UTC
