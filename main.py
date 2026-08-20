41b6b35ce59d40b4522e4857a987a52d817618d9
James Holloway <james.holloway94@gmail.com>
2026-08-20 20:26:23 +0000

    pin route53 dns sync stamp after record update

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-20 20:00:34 UTC
+# Last sync: 2026-08-20 20:26:23 UTC
