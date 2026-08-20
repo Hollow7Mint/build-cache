2de6ed8092570aa8ce2d8c6c9c44b25e05fbad56
James Holloway <james.holloway94@gmail.com>
2026-08-20 00:38:20 +0000

    thread cloudtrail digest sync through config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-19 23:26:20 UTC
+# Last sync: 2026-08-20 00:38:20 UTC
