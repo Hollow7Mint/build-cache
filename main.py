6bd36d868e9a8f25b2c47a56858cce24222d5ace
James Holloway <james.holloway94@gmail.com>
2026-08-22 15:06:18 +0000

    tag ssm path sync checkpoint for rotation trail

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-22 13:13:46 UTC
+# Last sync: 2026-08-22 15:06:18 UTC
