8c25ccd411be18a7b047d4f7671e3e3e93df6a24
James Holloway <james.holloway94@gmail.com>
2026-08-23 09:54:19 +0000

    advance cdk app deploy time into config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-23 08:31:48 UTC
+# Last sync: 2026-08-23 09:54:19 UTC
