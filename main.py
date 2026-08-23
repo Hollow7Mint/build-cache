02aae81fc32eeace00b421f57ded6d75ebbd7ef7
James Holloway <james.holloway94@gmail.com>
2026-08-23 12:11:53 +0000

    validate bastion access time in security config

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-23 11:12:33 UTC
+# Last sync: 2026-08-23 12:11:53 UTC
