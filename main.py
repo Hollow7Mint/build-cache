3c25b3bd7b636e8319a463699e5055972f080de8
James Holloway <james.holloway94@gmail.com>
2026-08-26 14:25:58 +0000

    bridge ssh ProxyJump config sync through banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-26 13:25:30 UTC
+# Last sync: 2026-08-26 14:25:58 UTC
