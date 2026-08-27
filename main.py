5c0883e5a8b19883c18a657d7946cdafd60a32ae
James Holloway <james.holloway94@gmail.com>
2026-08-27 19:27:43 +0000

    capture host key sync completion time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 18:50:39 UTC
+# Last sync: 2026-08-27 19:27:43 UTC
