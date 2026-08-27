7637a3989fdebb1ec1a0a53ee49ab3fce3735418
James Holloway <james.holloway94@gmail.com>
2026-08-27 13:05:24 +0000

    unify ec2 placement sync with metadata heartbeat

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-27 12:06:23 UTC
+# Last sync: 2026-08-27 13:05:24 UTC
