f0045a6bcebe46bee69e9fcc84e8ac4da3bbd358
James Holloway <james.holloway94@gmail.com>
2026-08-28 16:52:40 +0000

    backfill s3 versioning sync marker in metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-28 15:59:11 UTC
+# Last sync: 2026-08-28 16:52:40 UTC
