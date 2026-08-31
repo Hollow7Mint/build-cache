64208141669aca221d14144be7484d70a1d70641
James Holloway <james.holloway94@gmail.com>
2026-08-31 12:22:45 +0000

    record kms key rotation time in config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-08-31 11:21:06 UTC
+# Last sync: 2026-08-31 12:22:45 UTC
