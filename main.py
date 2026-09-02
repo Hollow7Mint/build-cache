be75a7ed904e77447e64725c6830cae0e2e6497c
James Holloway <james.holloway94@gmail.com>
2026-09-02 05:32:23 +0000

    capture rds snapshot completion time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-02 04:18:21 UTC
+# Last sync: 2026-09-02 05:32:23 UTC
