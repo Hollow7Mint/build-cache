9b0ba05cec90ece6fd3e7caab4c655888af8de65
James Holloway <james.holloway94@gmail.com>
2026-09-02 17:05:36 +0000

    mirror ssh certificate chain time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-02 15:24:09 UTC
+# Last sync: 2026-09-02 17:05:36 UTC
