c7970352ab439be63cee10368f39be9d7ab7b8df
James Holloway <james.holloway94@gmail.com>
2026-09-04 09:31:36 +0000

    frame ssm document sync time in config record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-04 08:26:02 UTC
+# Last sync: 2026-09-04 09:31:36 UTC
