ee289e1a05535a75ab1e6383c59b17f81b4b379b
James Holloway <james.holloway94@gmail.com>
2026-09-04 04:59:00 +0000

    lift db replica sync time into banner record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-04 03:55:19 UTC
+# Last sync: 2026-09-04 04:59:00 UTC
