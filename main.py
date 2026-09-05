499e31ac90285f0e9aa30dd530a73181189f8c00
James Holloway <james.holloway94@gmail.com>
2026-09-05 15:07:19 +0000

    transfer kms alias time to config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 13:22:31 UTC
+# Last sync: 2026-09-05 15:07:19 UTC
