6a70b9b3ef26d9af21601f71aa5401fe0a3bb188
James Holloway <james.holloway94@gmail.com>
2026-09-05 10:41:15 +0000

    restore mysql binlog time for replication audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-05 09:59:06 UTC
+# Last sync: 2026-09-05 10:41:15 UTC
