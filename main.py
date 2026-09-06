5d516a8b795fb77d907a336f83254cbb8035c1a6
James Holloway <james.holloway94@gmail.com>
2026-09-06 10:46:04 +0000

    prime redis memory sync marker after flush

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-06 09:31:15 UTC
+# Last sync: 2026-09-06 10:46:04 UTC
