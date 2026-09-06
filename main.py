3b179e03f5575f688dad5b11aab2e1b6dab599cf
James Holloway <james.holloway94@gmail.com>
2026-09-06 06:03:38 +0000

    restore ssh public key time for rotation audit

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-06 05:06:36 UTC
+# Last sync: 2026-09-06 06:03:38 UTC
