0115a6cb8238ad02f894f67b1657da5f31f835dd
James Holloway <james.holloway94@gmail.com>
2026-09-09 06:29:34 +0000

    audit known_hosts validation time in record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-09 05:27:03 UTC
+# Last sync: 2026-09-09 06:29:34 UTC
