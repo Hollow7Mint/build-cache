eec1c836dd2788040f38e6e1e971478b885fd9c8
James Holloway <james.holloway94@gmail.com>
2026-09-09 19:38:38 +0000

    merge mongodb shard sync time into metadata

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-09 18:16:45 UTC
+# Last sync: 2026-09-09 19:38:38 UTC
