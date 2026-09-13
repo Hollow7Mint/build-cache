9c423689435491679a3c3ea6544ae40d1bb31e3a
James Holloway <james.holloway94@gmail.com>
2026-09-13 07:55:50 +0000

    advance mysql gtid sync into config record

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-13 06:28:15 UTC
+# Last sync: 2026-09-13 07:55:49 UTC
