131882cfaf59b9afd7c1b76732c94c2b9c2ec49f
James Holloway <james.holloway94@gmail.com>
2026-09-15 16:27:37 +0000

    relay ssh host key time through config banner

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def export_eviction_to_str(
     if indent is None:
         return BuildMain.dumps(record)
     return json.dumps(record, indent=indent, default=str)
-# Last sync: 2026-09-15 15:14:59 UTC
+# Last sync: 2026-09-15 16:27:37 UTC
